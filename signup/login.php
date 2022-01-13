ad455a195f4788d0720ffc6693eacf4b428fc4d3
hosunghan-0821 <winsomed96@naver.com>
2022-01-13 10:18:45 +0000

    회원가입 1/13

---
+++ signup/login.php
@@ -0,0 +1,37 @@
+<?php
+
+    require_once $_SERVER['DOCUMENT_ROOT'].'/lib/dbConnect.php';
+    if(isset($_POST['id'])&&isset($_POST['password'])){
+
+        $id=$_POST['id'];
+        $password=$_POST['password'];
+
+        $sql="SELECT Member_id, Member_password FROM Market_member Where Member_id='$id'";
+        $selectResult= mysqli_query($db_connect,$sql);
+        $Data= mysqli_fetch_array($selectResult);
+
+        if($Data!=null){
+
+            if($Data['Member_password']===$password){
+                $arr['message']='로그인 성공';
+                echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+            }
+            else{
+                $arr['message']='로그인 실패';
+                echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+            }
+        }
+
+        else{
+            $arr['message']='로그인 실패';
+            echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+        }
+        
+    }
+    else{
+        $arr['message']='회원정보 부족 실패';
+        echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+
+    }
+
+?>
--- signup/signup.php
+++ signup/signup.php
@@ -2,11 +2,34 @@
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/lib/dbConnect.php';
 
-    if(!isset($_POST['nickname'])){
 
+    $asd=123;
+    if(isset($_POST['id'])){
+
+        $id=$_POST['id'];
+        $password=$_POST['password'];
+        $nickname=$_POST['nickname'];
+       
+        $sql="INSERT INTO Market_member (Member_id,Member_password,Member_nickname) Values('$id','$password','$nickname')";
+        $insertResult=mysqli_query($db_connect,$sql);
+        
+        if($insertResult){
+            $arr['message']="회원가입 성공";
+            echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+        }
+
+        else{
+            $arr['message']="회원가입 실패";
+            echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+        }
+    }
+    else{
+
+        $arr['message']="회원 정보 전달 안됨";
+        echo json_encode($arr,JSON_UNESCAPED_UNICODE);
     }
 
-    
+    $asd;
 
 
 
